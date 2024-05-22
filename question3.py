2119b46a47454c158b5c67390221ad831b4b33be
Vibisha21 <154110631+Vibisha21@users.noreply.github.com>
2024-05-22 10:23:46 +0530

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,1 +1,8 @@
 #write a program to find the factorial of a nummber
+def fact(n):
+    if(n==0 or n==1):
+        return 1
+    else:
+        return n*fact(n-1)
+n= int(input("Enter a number to find its factorial"))
+print(fact(n))
